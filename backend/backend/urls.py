d630c7a542a816e5093205d1c066bc0dade5a3c4
Wai Phyo Aung <waiphyoag.cs34@gmail.com>
2024-04-01 13:14:11 +0100

    [chores] remove otp feature

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('labtests/', include('labtests.urls')),
     path('healthconcerns/',include('healthconcerns.urls')),
     path('booking/',include('bookings.urls')),
-    path('otp/',include("otp.urls")),
     path('medicines/',include("medicines.urls"))
 ]
--- backend/otp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index,VerifyOTPView
-
-
-urlpatterns = [
-    path('get',index),
-    path('post',VerifyOTPView.as_view())
-]
